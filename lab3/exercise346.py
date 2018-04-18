5ae58c0301065e93482bc962c6a7fca4078215e8
yidiwang21 <ydwang21@gmail.com>
2018-04-18 09:29:35 -0700

    fixed trace problem with diag

--- lab3/exercise346.py
+++ lab3/exercise346.py
@@ -12,10 +12,9 @@ def se3Decide(se3mat):
                     [se3mat[1][0],se3mat[1][1],se3mat[1][2]],
                     [se3mat[2][0],se3mat[2][1],se3mat[2][2]]]
             print so3mat
-        if np.shape(se3mat) == (4,4) and ex.so3Decide(so3mat) == True:
-            pass
-        if se3mat[3][0] == 0 and se3mat[3][1] == 0 and se3mat[3][2] == 0 and se3mat[3][3] == 0:
-            return True
+            if np.shape(se3mat) == (4,4) and ex.so3Decide(so3mat) == True and \
+                se3mat[3][0] == 0 and se3mat[3][1] == 0 and se3mat[3][2] == 0 and se3mat[3][3] == 0:
+                    return True
         else:
             return Flase
     except:
--- lab3/rotmat.py
+++ lab3/rotmat.py
@@ -2,18 +2,28 @@
 import modern_robotics as mr
 import numpy as np
 from math import cos, acos, sin, tan, pi, sqrt
+from scipy.linalg import lu
 
 epsilon = 0.05
 DET_MAX = 1 + epsilon
 DET_MIN = 1 - epsilon
 
+def MatDiag(A):
+    pl, u = lu(A, permute_l=True)
+    return u
+
 def RotDecide(R):
     try:
         if np.linalg.det(R) <= DET_MAX and np.linalg.det(R) >= DET_MIN:
-            pass
-        if np.matrix.trace(np.linalg.inv(R)) - np.matrix.trace(mr.RotInv(R)) <= epsilon:
-            return True
+            if np.matrix.trace(MatDiag(np.linalg.inv(R))) - np.matrix.trace(MatDiag(mr.RotInv(R))) <= epsilon:
+                return True
         else:
             return False
     except:
         print("Failed to calculate!")
+
+R = np.array([[0.8378, 0.0940, 0.5378],
+            [0.0071, 0.9831, -0.1830],
+            [-0.5459, 0.1572, 0.8230]])
+
+print np.linalg.det(R), RotDecide(R), MatDiag(R), np.matrix.trace(MatDiag(R))
