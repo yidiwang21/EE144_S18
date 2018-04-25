00525070a05bcc74d61c69a6fdd732cd49166ba4
yidiwang21 <ydwang21@gmail.com>
2018-04-25 10:55:15 -0700

    add trajectory plotting add some comments and remove some useless code

--- lab4/p_control.py
+++ lab4/p_control.py
@@ -6,23 +6,22 @@ from std_msgs.msg import String, Empty
 from time import time
 import geometry_msgs.msg
 import tf
+import numpy as np
+import matplotlib.pyplot as plt
 
 PI = 3.1415926535897
 PHI = 0
-kp = 20
+kp = 18     # set an estimated proper kp value
+x = np.array([0])
+y = np.array([0])
 
+# Initialize the tf listener
 tfListener = tf.TransformListener()
 
 class turtlebot_move():
     def __init__(self):
         # reset the position of the robot to the center of the world for easier observaton
         rospy.init_node('reset_world', anonymous=False)
-        # rospy.wait_for_service('/gazebo/reset_world')
-        # reset_world = rospy.ServiceProxy('/gazebo/reset_world',Empty)
-        # if reset_world():
-        #     rospy.loginfo("World successfully reset!")
-        # else:
-        #     rospy.loginfo("World failed to reset!")
 
         rospy.loginfo("Press CTRL + C to terminate")
         rospy.on_shutdown(self.shutdown)
@@ -34,15 +33,12 @@ class turtlebot_move():
         timer = time()
         while time() - timer < 1.0:
             reset_odom.publish(Empty())
-        # Initialize the tf listener
 
         rospy.loginfo("Finished Initializing!")
 
     def moveForward(self):
         rospy.loginfo("Start moving forward...")
-
-        # angular_speed = 10*2*PI/360             # pick a proper angular speed
-        # relative_angle = 90*2*PI/360            # set target turning angle to 90 degrees
+        global x, y
 
         vel = Twist()
         vel.linear.x = 0.5
@@ -53,27 +49,33 @@ class turtlebot_move():
         vel.angular.z = 0
 
         (position, quaternion) = tfListener.lookupTransform("/odom", "/base_footprint", rospy.Time(0))
-        orientation_set = tf.transformations.euler_from_quaternion(quaternion)
+        orientation = tf.transformations.euler_from_quaternion(quaternion)
+        init_orientation = orientation[2]
+        print 'init_orientation = ', init_orientation
 
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             t1 = rospy.Time.now().to_sec()
             t = rospy.Time(t1 + 10)
-            cnt = 0
             print("Current time:", t1)
+            cnt = 0
             while t1 < t.to_sec():
+                # get current position and orientation
                 (position, quaternion) = tfListener.lookupTransform("/odom", "/base_footprint", rospy.Time(0))
                 orientation = tf.transformations.euler_from_quaternion(quaternion)
-                rospy.loginfo("position: "+str(position))
-                rospy.loginfo("orientation: "+str(orientation))
-                det_phi = abs(orientation[2] - orientation_set[2])
+                # rospy.loginfo("position: " + str(position))
+                # rospy.loginfo("orientation: " + str(orientation))
+                if cnt % 3 == 0:
+                    x = np.append(x, position[0])
+                    y = np.append(y, position[1])
+                cnt = cnt + 1
+                # get the orientation difference of z-axis
+                det_phi = init_orientation - orientation[2]
+                # p controller of angular speed of z-axis
+                vel.angular.z = kp * det_phi * 2 * PI/360
                 self.set_velocity.publish(vel)
                 rate.sleep()
                 t1 = rospy.Time.now().to_sec()
-                # if cnt % 10 == 0:
-                #     print("Current time:", t1)  # print current time every 10 loops
-                cnt = cnt + 1
-                vel.angular.z = kp*det_phi*2*PI/360
             break                               # after 10 secs, end moving forward
 
         rospy.sleep(1)
@@ -110,8 +112,6 @@ class turtlebot_move():
 
         rospy.sleep(1)
 
-
-
     def shutdown(self):
         rospy.loginfo("Stop Action")
         stop_vel = Twist()
@@ -125,10 +125,13 @@ if __name__ == '__main__':
     try:
         ins = turtlebot_move()
         i = 0
-        while i < 100:            # iter 4 times to complete a square
+        while i < 4:            # iter multiple times just for test and plotting
             ins.moveForward()
             ins.turnRight()
             i = i + 1
+            PHI = i * PI / 2
         rospy.loginfo("Finished Moving 5x5")
+        plt.scatter(x, y)
+        plt.show()
     except rospy.ROSInterruptException or KeyboardInterrupt:
         rospy.loginfo("Action terminated.")
