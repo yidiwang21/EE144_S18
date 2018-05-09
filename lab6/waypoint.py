28ec73fd96b450094a283d6372c4dcece57e14f1
YidiWang <ydwang21@gmail.com>
2018-05-08 19:46:33 -0700

    add lab6

---
+++ lab6/waypoint.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 16:26:36 2018
+
+@author: ee144
+"""
+#these waypoints are given as list for convience, however, you can use any data type that you like
+#i.e. np.array()
+#as long as you use the same set of way points as below.
+#These coordinates are in the "world" coordinate frame
+
+import numpy as np
+
+waypoints = np.array([[0,0],[0.5,0],[1,0],[1,0],[1,0.5],[1,1],[1,1],[0.5,1],[0,1],[0,1],[0,0.5],[0,0]])
+
